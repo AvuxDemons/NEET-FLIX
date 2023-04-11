3fe11643ee93f3f99a6ae5a35e0193e7a73ab492
Ilyrp <ilyasaputra8@gmail.com>
2023-04-11 14:40:55 +0700

    struct

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type User struct {
+	nama        string
+	email       string
+	membership  string
+	tangglLahir int
+}
+
+type Movie struct {
+	judul      string
+	tahunRilis int
+	genre      string
+	kategori   string
+	studio     string
+	rating     float32
+	rateUmur   string
+}
+
+func main() {
+	fmt.Println("hehhh")
+}
