1270478832525fe90dcc19483d7f8bc6e3992cb7
Fardanaya <frankyardanaya@gmail.com>
2023-04-11 16:11:48 +0700

    fungsi sudah masuk,(-) view by kategori,genre,umur

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type User struct {
-	nama        string
+	username    string
 	email       string
 	membership  string
 	tangglLahir int
@@ -19,6 +24,296 @@ type Movie struct {
 	rateUmur   string
 }
 
+type structUser struct {
+	data User
+	next *structUser
+}
+
+type structMovie struct {
+	data Movie
+	next *structMovie
+}
+
+var id int = 0
+var user structUser
+var movie structMovie
+
+func InsertData(listUser *structUser, listMovie *structMovie, user User, movie Movie, selector int) {
+	var tempUser structUser
+	var tempMovie structMovie
+	if selector == 1 {
+		tempUser.data = user
+		temp := listUser
+
+		if temp.next == nil {
+			temp.next = &tempUser
+		} else {
+			for temp.next != nil {
+				temp = temp.next
+			}
+			temp.next = &tempUser
+		}
+	} else {
+		tempMovie.data = movie
+		temp := listMovie
+
+		if temp.next == nil {
+			temp.next = &tempMovie
+		} else {
+			for temp.next != nil {
+				temp = temp.next
+			}
+			temp.next = &tempMovie
+		}
+	}
+}
+
+func DeleteData(listUser *structUser, listMovie *structMovie, selector int, userId string) {
+	tempUser := listUser
+	tempMovie := listMovie
+
+	var counter int = 0
+	if selector == 1 {
+		for tempUser.next != nil {
+			counter++
+			if tempUser.next.data.username == userId {
+				if counter == 1 {
+					tempUser.next = tempUser.next.next
+					break
+				} else if tempUser.next.next != nil {
+					tempUser.next = tempUser.next.next
+					break
+				} else {
+					tempUser.next = nil
+					break
+				}
+			}
+			tempUser = tempUser.next
+		}
+	} else {
+		for tempMovie.next != nil {
+			counter++
+			if tempMovie.next.data.judul == userId {
+				if counter == 1 {
+					tempMovie.next = tempMovie.next.next
+					break
+				} else if tempMovie.next.next != nil {
+					tempMovie.next = tempMovie.next.next
+					break
+				} else {
+					tempMovie.next = nil
+					break
+				}
+			}
+			tempMovie = tempMovie.next
+		}
+	}
+}
+
+func UpdateData(listUser *structUser, listMovie *structMovie, user User, movie Movie, selector int) {
+	tempUser := listUser
+	tempMovie := listMovie
+
+	if selector == 1 {
+		for tempUser.next != nil {
+			if tempUser.next.data.username == user.username {
+				tempUser.next.data = user
+				break
+			}
+			tempUser = tempUser.next
+		}
+	} else {
+		for tempMovie.next != nil {
+			if tempMovie.next.data.judul == movie.judul {
+				tempMovie.next.data = movie
+				break
+			}
+			tempMovie = tempMovie.next
+		}
+	}
+}
+
+func ReadData(listUser *structUser, listMovie *structMovie, selector int, sort string) {
+	tempUser := listUser.next
+	tempMovie := listMovie.next
+
+	fmt.Print("View Data ")
+	if selector == 1 {
+		fmt.Println("Dosen")
+	} else {
+		fmt.Println("Mahasiswa")
+	}
+
+	if sort == "nil" {
+		// View All
+		if selector == 1 {
+			for tempUser != nil {
+				fmt.Println("-Username\t : ", tempUser.data.username)
+				fmt.Println("-Email\t : ", tempUser.data.email)
+				fmt.Println("-Membership\t : ", tempUser.data.membership)
+				fmt.Println("-Tanggal Lahir\t : ", tempUser.data.tangglLahir)
+				tempUser = tempUser.next
+			}
+		} else {
+			for tempMovie != nil {
+				fmt.Println("-Judul\t : ", tempMovie.data.judul)
+				fmt.Println("-Tahun Rilis\t : ", tempMovie.data.tahunRilis)
+				fmt.Println("-Genre\t : ", tempMovie.data.genre)
+				fmt.Println("-Kategori\t : ", tempMovie.data.kategori)
+				fmt.Println("-Studio\t : ", tempMovie.data.studio)
+				fmt.Println("-Rating\t : ", tempMovie.data.rating)
+				fmt.Println("-Rate Umur\t : ", tempMovie.data.rateUmur)
+				tempMovie = tempMovie.next
+			}
+		}
+	} else {
+		// View By Username or Judul
+		if selector == 1 {
+			for tempUser != nil {
+				if tempUser.data.username == sort {
+					fmt.Println("-Username\t : ", tempUser.data.username)
+					fmt.Println("-Email\t : ", tempUser.data.email)
+					fmt.Println("-Membership\t : ", tempUser.data.membership)
+					fmt.Println("-Tanggal Lahir\t : ", tempUser.data.tangglLahir)
+				}
+				tempUser = tempUser.next
+			}
+		} else {
+			for tempMovie != nil {
+				if tempMovie.data.judul == sort {
+					fmt.Println("-Judul\t : ", tempMovie.data.judul)
+					fmt.Println("-Tahun Rilis\t : ", tempMovie.data.tahunRilis)
+					fmt.Println("-Genre\t : ", tempMovie.data.genre)
+					fmt.Println("-Kategori\t : ", tempMovie.data.kategori)
+					fmt.Println("-Studio\t : ", tempMovie.data.studio)
+					fmt.Println("-Rating\t : ", tempMovie.data.rating)
+					fmt.Println("-Rate Umur\t : ", tempMovie.data.rateUmur)
+				}
+				tempMovie = tempMovie.next
+			}
+		}
+	}
+}
+
+func inputUser(selector int, model int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	var x string
+	var a, b, c, d, e string //agar lebih hemat variabel, saya pake di input user dan movienya
+	var tangglLahir, tahunRilis int
+	var rating float32
+
+	scanner.Scan()
+
+	if model == 1 {
+
+		for x != "t" {
+			// scanner := bufio.NewScanner(os.Stdin)
+			fmt.Print("Masukkan Data ")
+			if selector == 1 {
+				fmt.Println("User")
+				fmt.Print("Username\t: ")
+				scanner.Scan()
+				a = scanner.Text()
+				fmt.Print("Email\t: ")
+				scanner.Scan()
+				b = scanner.Text()
+				fmt.Print("Membership\t: ")
+				scanner.Scan()
+				c = scanner.Text()
+				fmt.Print("Tanggal Lahir\t: ")
+				scanner.Scan()
+				fmt.Scan(&tangglLahir)
+			} else {
+				fmt.Println("Movie")
+				fmt.Print("Judul\t: ")
+				scanner.Scan()
+				a = scanner.Text()
+				fmt.Print("Tahun Rilis\t: ")
+				scanner.Scan()
+				fmt.Scan(&tahunRilis)
+				fmt.Print("Genre\t: ")
+				scanner.Scan()
+				b = scanner.Text()
+				fmt.Print("Kategori\t: ")
+				scanner.Scan()
+				c = scanner.Text()
+				fmt.Print("Studio\t: ")
+				scanner.Scan()
+				d = scanner.Text()
+				fmt.Print("Rating\t: ")
+				scanner.Scan()
+				fmt.Scan(&rating)
+				fmt.Print("Rate Umur\t: ")
+				scanner.Scan()
+				e = scanner.Text()
+			}
+
+			id++
+
+			// Data Skeleton
+			dataUser := User{username: a, email: b, membership: c, tangglLahir: tangglLahir}
+			dataMovie := Movie{judul: a, tahunRilis: tahunRilis, genre: b, kategori: c, studio: d, rating: rating, rateUmur: e}
+
+			// Insert Data
+			InsertData(&user, &movie, dataUser, dataMovie, selector)
+
+			// Break Looping
+			fmt.Println("TAMBAH DATA ? [ y / t ]")
+			scanner.Scan()
+			x = strings.TrimSpace(scanner.Text())
+		}
+	} else {
+		fmt.Print("Masukkan Data Baru ")
+		if selector == 1 {
+			fmt.Println("User")
+			fmt.Print("Username\t: ")
+			scanner.Scan()
+			a = scanner.Text()
+			fmt.Print("Email\t: ")
+			scanner.Scan()
+			b = scanner.Text()
+			fmt.Print("Membership\t: ")
+			scanner.Scan()
+			c = scanner.Text()
+			fmt.Print("Tanggal Lahir\t: ")
+			scanner.Scan()
+			fmt.Scan(&tangglLahir)
+		} else {
+			fmt.Println("Movie")
+			fmt.Print("Judul\t: ")
+			scanner.Scan()
+			a = scanner.Text()
+			fmt.Print("Tahun Rilis\t: ")
+			scanner.Scan()
+			fmt.Scan(&tahunRilis)
+			fmt.Print("Genre\t: ")
+			scanner.Scan()
+			b = scanner.Text()
+			fmt.Print("Kategori\t: ")
+			scanner.Scan()
+			c = scanner.Text()
+			fmt.Print("Studio\t: ")
+			scanner.Scan()
+			d = scanner.Text()
+			fmt.Print("Rating\t: ")
+			scanner.Scan()
+			fmt.Scan(&rating)
+			fmt.Print("Rate Umur\t: ")
+			scanner.Scan()
+			e = scanner.Text()
+		}
+
+		id++
+
+		// Data Skeleton
+		dataUser := User{username: a, email: b, membership: c, tangglLahir: tangglLahir}
+		dataMovie := Movie{judul: a, tahunRilis: tahunRilis, genre: b, kategori: c, studio: d, rating: rating, rateUmur: e}
+
+		// Update Data
+		UpdateData(&user, &movie, dataUser, dataMovie, selector)
+	}
+}
+
 func main() {
-	fmt.Println("hehhh")
+
 }
